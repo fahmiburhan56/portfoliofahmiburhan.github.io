e58e3d086101570a6457c25a89583e8aeb2f241d
fahmiburhan56 <fahmiburhan56@gmail.com>
2024-02-22 11:38:37 +0700

    inisial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    
+    extend: {
+      colors:{
+        primary:'#38bdf8',
+        secondary: '#64748b',
+        dark:'#0f172a',
+      },
+      fontFamily:{
+        lexend:['Lexend'],protest:['Protest Guerrilla'],
+      },
+    },
+  },
+  plugins: [],
+}
+
